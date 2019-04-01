2f99b2019c0532c21142986c31ce322d418a7460
akralj <dev@kralj.de>
2019-04-01 12:33:44 +0200

    first version working as gh-pages

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+npm run site:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd portfolio/.vuepress/dist/
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
+# needs to create new repo called <USERNAME>.github.io
 git push -f git@github.com:akralj/akralj.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -2,6 +2,7 @@ const config = require('../config.json');
 module.exports = {
   title: config.title,
   description: config.description,
+  //base: "/portfolio-vuepress/",
   base: "/",
   themeConfig: {
     logo: config.logo,
